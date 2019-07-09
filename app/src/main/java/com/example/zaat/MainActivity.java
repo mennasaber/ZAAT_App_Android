680e7f3a8b244ec80f95d4dd75b19117989a6259
Menna Saber <51786367+mennasaber@users.noreply.github.com>
2019-07-10 01:31:30 +0200

    Connect ZAAT with Database 'Firebase'

--- app/src/main/java/com/example/zaat/MainActivity.java
+++ app/src/main/java/com/example/zaat/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.zaat;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    Members members;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPageAdapter);
         TabLayout tab = findViewById(R.id.tabs);
         tab.setupWithViewPager(viewPager);
+        members = new Members();
+        members.setAge(5);
+        members.setName("menna");
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        databaseReference.push().setValue(members);
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
     }
 }
